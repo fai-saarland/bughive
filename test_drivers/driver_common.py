c503e510d08d72b62e13b6f026f841ee0a34ed50
Jan Eisenhut <eisenhut@cs.uni-saarland.de>
2024-02-23 22:25:59 +0100

    improved mem limit handling in driver script; allow different mem limit for fd process

--- test_drivers/driver_common.py
+++ test_drivers/driver_common.py
@@ -13,30 +13,52 @@ def add_common_args(parser):
     parser.add_argument("--search", required=True, help="Search configuration of FD")
     parser.add_argument("-t", "--timeout", type=int, help="Time limit for the driver in seconds")
     parser.add_argument("-m", "--mem", type=int, help="Memory limit for the driver in MiB")
+    parser.add_argument("--fdmem", type=int, help="Set maximal memory limit for FD process in MiB")
 
 
 def run_driver(server_command, args):
     script_start = time.time()
     return_code = 3
-
-    # get the available memory and leave margin for driver itself
+    soft_mem_limit, hard_mem_limit = resource.getrlimit(resource.RLIMIT_AS)
+    print(f"read memory resource limits of driver process (in bytes): " 
+          f"soft={soft_mem_limit if soft_mem_limit >= 0 else 'unlimited'}, " 
+          f"hard={hard_mem_limit if hard_mem_limit >= 0 else 'unlimited'}")
+    policy_mem_limit = None
+    fd_mem_limit = None
     set_mem_limit = False
     if args.mem is not None:
-        available_mem = args.mem * 1024 * 1024 - 10
+        available_mem = args.mem * 1024 * 1024 - 1024 * 1024
         set_mem_limit = True
     else:
-        current_rlimit = resource.getrlimit(resource.RLIMIT_AS)[0]
-        if current_rlimit >= 0:
-            available_mem = current_rlimit - 10
+        if soft_mem_limit >= 0:
+            available_mem = soft_mem_limit - 1024 * 1024
             set_mem_limit = True
     if set_mem_limit:
-        if available_mem <= 2:
+        if available_mem <= 1024 * 1024:
             print("Not enough memory provided. Server and FD not started.")
             sys.exit(5)
-        internal_mem_limit = (available_mem // 2)  # memory limit for server and client each
-        resource.setrlimit(resource.RLIMIT_AS, (internal_mem_limit, internal_mem_limit))
+        if args.fdmem:
+            fd_mem_limit = min(available_mem, args.fdmem * 1024 * 1024)
+            policy_mem_limit = available_mem - fd_mem_limit
+            if fd_mem_limit <= 1024*1024 or policy_mem_limit <= 1024*1024:
+                print(f"FD or policy mem limit not high enough.")
+                sys.exit(5)
+        else:
+            policy_mem_limit = (available_mem // 2)
+            fd_mem_limit = policy_mem_limit
 
-    with subprocess.Popen(server_command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT) as server_process:
+    def set_policy_mem_limit():
+        if policy_mem_limit:
+            print(f"limiting policy server process to {policy_mem_limit} bytes", flush=True)
+            resource.setrlimit(resource.RLIMIT_AS, (policy_mem_limit, hard_mem_limit))
+
+    def set_fd_mem_limit():
+        if fd_mem_limit:
+            print(f"limiting fd process to {fd_mem_limit} bytes", flush=True)
+            resource.setrlimit(resource.RLIMIT_AS, (fd_mem_limit, hard_mem_limit))
+    
+    print("Starting policy server")
+    with subprocess.Popen(server_command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, preexec_fn=set_policy_mem_limit) as server_process:    
         port = 0
         for line in server_process.stdout:
             decoded_line = line.decode()
@@ -59,7 +81,7 @@ def run_driver(server_command, args):
                     sys.exit(4)
                 print("Starting FD process", flush=True)
                 fd_process = subprocess.run([args.downward, "--remote-policy", f"localhost:{port}", "--search", args.search],
-                                            timeout=args.timeout)
+                                            timeout=args.timeout, preexec_fn=set_fd_mem_limit)
                 return_code = fd_process.returncode
                 print(f"FD process terminated with return code {return_code}", flush=True)
             except subprocess.TimeoutExpired:
