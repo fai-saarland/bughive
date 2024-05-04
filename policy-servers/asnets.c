cb979bba6ec2bca8707b9529cf71f1bfcb2864f7
Daniel Fiser <danfis@danfis.cz>
2024-05-04 09:17:14 +0200

    Updated version of cpddl

--- policy-servers/asnets.c
+++ policy-servers/asnets.c
@@ -4,7 +4,7 @@
 
 pddl_err_t err = PDDL_ERR_INIT;
 pddl_asnets_t *asnets = NULL;
-const pddl_asnets_ground_task_t *task = NULL;
+pddl_asnets_ground_task_t task;
 
 static char *reqFDRTaskFD(size_t *size, void *data)
 {
@@ -14,7 +14,7 @@ static char *reqFDRTaskFD(size_t *size, void *data)
     pddl_fdr_write_config_t write_cfg = PDDL_FDR_WRITE_CONFIG_INIT;
     write_cfg.fout = fout;
     write_cfg.use_fd_fact_names = 1;
-    pddlFDRWrite(&task->fdr, &write_cfg);
+    pddlFDRWrite(&task.fdr, &write_cfg);
     fflush(fout);
     fclose(fout);
     return out;
@@ -22,7 +22,7 @@ static char *reqFDRTaskFD(size_t *size, void *data)
 
 static int reqFDROperator(const int *state, void *data)
 {
-    return pddlASNetsRunPolicy(asnets, task, state, NULL);
+    return pddlASNetsRunPolicy(asnets, &task, state, NULL, NULL);
 }
 
 static int reqFDROperatorsProb(const int *state,
@@ -33,7 +33,7 @@ static int reqFDROperatorsProb(const int *state,
 {
     pddl_asnets_policy_distribution_t op_dist;
     pddlASNetsPolicyDistributionInit(&op_dist);
-    int st = pddlASNetsPolicyDistribution(asnets, task, state, &op_dist);
+    int st = pddlASNetsPolicyDistribution(asnets, &task, state, NULL, &op_dist);
     if (st == 0){
         *op_size = op_dist.op_size;
         *op_ids = (int *)malloc(sizeof(int) * *op_size);
@@ -63,22 +63,20 @@ int main(int argc, char *argv[])
     pddlErrInit(&err);
     pddlErrLogEnable(&err, stdout);
 
-    pddl_asnets_config_t cfg;
-    pddlASNetsConfigInitFromModel(&cfg, policy_file, &err); 
-    pddlASNetsConfigSetDomain(&cfg, domain_file);
-    pddlASNetsConfigAddProblem(&cfg, problem_file); 
-
-    asnets = pddlASNetsNew(&cfg, &err);
+    asnets = pddlASNetsNewLoad(policy_file, domain_file, &err);
     if (asnets == NULL){
         pddlErrPrint(&err, 1, stderr);
         return -1;
     }
 
-    if (pddlASNetsLoad(asnets, policy_file, &err) != 0){
+    const pddl_asnets_config_t *cfg = pddlASNetsGetConfig(asnets);
+    const pddl_asnets_lifted_task_t *lt = pddlASNetsGetLiftedTask(asnets);
+    if (pddlASNetsGroundTaskInit(&task, lt, domain_file, problem_file,
+                                 cfg, &err) != 0){
+        pddlASNetsDel(asnets);
         pddlErrPrint(&err, 1, stderr);
         return -1;
     }
-    task = pddlASNetsGetGroundTask(asnets, 0);
 
     return phrmPolicyServer(url, reqFDRTaskFD, reqFDROperator,
                             reqFDROperatorsProb, NULL);
